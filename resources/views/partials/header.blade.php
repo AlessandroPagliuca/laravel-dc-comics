e9af98a73beec3ef72558b03892abb2b45a95957
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-05-29 19:53:23 +0200

    Upgraded header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,30 @@
 <header>
-    <h1>header</h1>
+    <!-- dc power visa additional dc sites -->
+    <div class="bg-primary box-visa">
+        <div class="container d-flex justify-content-end align-items-center">
+            <div class="text-uppercase text-white px-4">dc power visa</div>
+            <div class="text-uppercase text-white px-4">additional dc sites</div>
+        </div>
+    </div>
+    <!-- navbar -->
+    <nav class="py-4">
+        <div class="container d-flex justify-content-center align-items-center">
+            <div class="box-logo">
+                <img src="{{ asset('resources/img/dc-logo.png') }}" alt="dc-comics">
+            </div>
+
+            <ul class="d-flex">
+                @if (is_array($headerLinks))
+                    @foreach ($headerLinks as $link)
+                        <li class="text-uppercase fw-semibold">{{ $link }}</li>
+                    @endforeach
+                @else
+                    <li class="text-uppercase fw-semibold">Invalid header links</li>
+                @endif
+            </ul>
+        </div>
+    </nav>
 </header>
+<div id="hero">
+    <img src=" {{ asset('resources/img/jumbotron.jpg') }} " alt="">
+</div>
