9cf4d7acde084ec3abd3b4faca97f3a2a0348d37
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-05-29 19:53:38 +0200

    Upgraded footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,38 @@
-<footer>
-    <h1>footer</h1>
+<footer class="bg-dark">
+    <div class="container d-flex text-white">
+        <div class="w-50 py-5">
+            <div class="row justify-content-start">
+                @if (is_array($headerLinks))
+                    @foreach ($footerLinks as $key => $value)
+                        <div class="col-6 col-sm-4 ">
+                            <h5 class=text-uppercase>{{ $value['title'] }}</h5>
+                            <ul>
+                                @if (is_array($footerLinks))
+                                    @foreach ($value['links'] as $link)
+                                        <li class="text-secondary text-capitalize">{{ $link }}</li>
+                                    @endforeach
+                                @else
+                                    <li class="text-uppercase fw-semibold">Invalid footer links</li>
+                                @endif
+                            </ul>
+                        </div>
+                    @endforeach
+                @else
+                    <li class="text-uppercase fw-semibold">Invalid footer links</li>
+                @endif
+            </div>
+        </div>
+
+        <div class="w-50">
+            <div class="box-img">
+                <img src=" {{ asset('resources/img/dc-logo-bg.png') }} " alt="">
+            </div>
+
+        </div>
+
+
+    </div>
+
+
+
 </footer>
