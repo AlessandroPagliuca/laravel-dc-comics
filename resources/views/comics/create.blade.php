6561feea6a5f3a00253e4c09a5912749cbabe678
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-05-31 16:15:19 +0200

    Upgrade error message and validation create

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -7,49 +7,86 @@
             @csrf
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
-                <input type="text" class="form-control" name="title" id="title" aria-describedby="titleComic" required>
-                <div id="titleComic" class="alert alert-danger fw-semibold">Enter title</div>
-
+                <input type="text" class="form-control @error('title') is-invalid
+                @enderror" name="title"
+                    id="title" aria-describedby="titleComic" value=" {{ old('title') }} ">
+                @error('title')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
             <div class="mb-3">
-                <label for="description" class="form-label">Enter description</label>
-                <textarea name="description" id="description" cols="30" rows="10" aria-describedby="descriptionComic">
+                <label for="description" class="form-label">Description</label>
+                <textarea name="description" id="description" class="@error('description') is-invalid
+                @enderror"
+                    cols="30" rows="10" aria-describedby="descriptionComic">
+
                 </textarea>
-                <div id="descriptionComic" class="alert alert-danger fw-semibold">Enter description</div>
-
+                @error('description')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
             <div class="mb-3">
-                <label for="thumb" class="form-label">Enter thumb</label>
-                <textarea name="thumb" id="thumb" cols="30" rows="10" aria-describedby="thumbComic">
+                <label for="thumb" class="form-label">Thumb</label>
+                <textarea name="thumb" id="thumb" class="@error('thumb') is-invalid
+                @enderror" cols="30"
+                    rows="10" aria-describedby="thumbComic">
+
                 </textarea>
-                <div id="thumbComic" class="alert alert-danger fw-semibold">Enter thumb</div>
+                @error('thumb')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="price" class="form-label">Price</label>
+                <input type="text" class="form-control @error('price') is-invalid
+                @enderror"
+                    name="price" id="price" aria-describedby="priceComic" value=" {{ old('price') }} ">
+                @error('price')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="series" class="form-label"> Series</label>
+                <input type="text" class="form-control @error('series') is-invalid
+                @enderror"
+                    name="series" id="series" aria-describedby="seriesComic" value=" {{ old('series') }} ">
+                @error('series')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="sale_date" class="form-label"> Sale date</label>
+                <input type="date" class="form-control @error('sale_date') is-invalid
+                @enderror"
+                    name="sale_date" id="sale_date" aria-describedby="sale_dateComic" value=" {{ old('sale_date') }} ">
+                @error('sale_date')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="type" class="form-label"> Type</label>
+                <input type="text" class="form-control @error('type') is-invalid
+                @enderror"
+                    name="type" id="type" aria-describedby="typeComic" value=" {{ old('type') }} ">
+                @error('type')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
 
-            </div>
-            <div class="mb-3">
-                <label for="price" class="form-label">Enter price</label>
-                <input type="text" class="form-control" name="price" id="price" aria-describedby="priceComic"
-                    required>
-                <div id="priceComic" class="alert alert-danger fw-semibold">Enter price</div>
-
-            </div>
-            <div class="mb-3">
-                <label for="series" class="form-label"> Enter series</label>
-                <input type="text" class="form-control" name="series" id="series" aria-describedby="seriesComic"
-                    required>
-                <div id="seriesComic" class="form-text">Enter series</div>
-            </div>
-            <div class="mb-3">
-                <label for="sale_date" class="form-label"> Enter sale date</label>
-                <input type="date" class="form-control" name="sale_date" id="sale_date" aria-describedby="sale_dateComic"
-                    required>
-                <div id="sale_dateComic" class="alert alert-danger fw-semibold">Enter sale date</div>
-            </div>
-            <div class="mb-3">
-                <label for="type" class="form-label"> Enter type</label>
-                <input type="text" class="form-control" name="type" id="type" aria-describedby="typeComic"
-                    required>
-                <div id="typeComic" class="alert alert-danger fw-semibold">Enter type</div>
-            </div>
 
             <button type="submit" class="valid-form btn btn-primary">Submit</button>
             <button type="reset" class="btn btn-warning">Reset</button>
